d41125698797d3d727168c2a0e9c909f8bc36b6a
Mike Filicicchia <33882347+TheMathNinja@users.noreply.github.com>
2026-09-03 19:51:42 -0400

    Exclude nominations from pre-UFA participation unless source identifies GM nomination

--- R/offseason_inactivity_monitor.R
+++ R/offseason_inactivity_monitor.R
@@ -346,7 +346,7 @@ pre_ufa_auction_participation_events <- function(bids, config) {
 pre_ufa_auction_participation_detail <- function(bids, config, franchises) {
   events <- pre_ufa_auction_participation_events(bids, config)
   participation_events <- events |>
-    filter(.data$event_kind == "bid" | (.data$event_kind == "nomination" & !.data$commissioner_initiated))
+    filter(.data$event_kind == "bid")
   franchise_order <- franchises |> transmute(franchise, franchise_sort_order = row_number())
 
   franchises |>
